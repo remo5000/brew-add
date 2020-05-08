e4a75d0b666e351b04abd982272fd84c6434c13e
Vignesh Shankar <vigneshshankar@gmail.com>
2020-05-08 12:32:49 +0800

    Fix things minorly

--- brew-add.rb
+++ brew-add.rb
@@ -10,20 +10,19 @@ module Homebrew
 
   def add_command_args!
     Homebrew::CLI::Parser.new do
-      banner_text = <<~EOS
+      usage_banner <<~EOS
         A helper to add a module to a Brewfile and install it at the same time.
         Usage: `add` <formula> <options>
 
         Preference goes to installing via `brew bundle install` and Brewfile location is
         assumed to be ~/Brewfile, unless otherwise specified (using --brewfile=<path>).
       EOS
-      usage_banner banner_text
       flag   '-b',
              '--brewfile=',
              description: 'Use a custom Brewfile, instead of ~/Brewfile'
       switch '-n',
              '--no-bundle-install',
-             description: 'Adds file to Brewfile without calling `bundle install`. '\
+             description: 'Adds file(s) to Brewfile without calling `bundle install`. '\
              'Saves time, but not recommended'
       switch '-f',
              '--force-bundle-install',
@@ -47,7 +46,7 @@ module Homebrew
 
     new_formulae_added = false
     args.formulae.each do |formula|
-      if brewfile_contents.include? formula.name
+      if brewfile_contents.include? "\"#{formula.name}\""
         puts "#{formula} already exists in Brewfile @ #{brewfile}"
       else
         system("echo 'brew \"#{formula.name}\"' >> #{brewfile}")
@@ -58,7 +57,7 @@ module Homebrew
 
     # Run brew bundle if new formulae added, but not if no_bundle_install specified
     unless args.force_bundle_install? ||
-        (!args.no_bundle_install? && new_formulae_added)
+           (!args.no_bundle_install? && new_formulae_added)
       return
     end
 
